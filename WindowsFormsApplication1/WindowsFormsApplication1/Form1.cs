02e7fcfb6c91ae181681772667b7568a8b17c150
avishka umayanga <35651169+umayanga1997@users.noreply.github.com>
2020-02-08 01:00:42 +0530

    Add files via upload

---
+++ WindowsFormsApplication1/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using clDll;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Additional ad = new Additional();
+            label1.Text = ad.addition(10, 20).ToString();
+        }
+    }
+}
---
+++ clDll/clDll/Additional.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace clDll
+{
+    public class Additional
+    {
+
+        public int addition(int x, int y) {
+            int total = x + y;
+            return total;
+        }
+    }
+}
